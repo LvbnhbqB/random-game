c75c67010a1185a03fce44240790a61f57b91fa7
Dmitriy Bekesh <bekeshdmitriy@yandex.ru>
2023-08-13 23:14:15 +0300

    feat: add burger-menu js

---
+++ js/main.js
@@ -0,0 +1,43 @@
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    const close = document.querySelector('.header_burger_close');
+    const closeItem = document.querySelector('.header_burger_close');
+    const openItem = document.querySelector('.header_burger');
+    const menuLinks = document.querySelectorAll('.nav-link');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+        openItem.classList.add('header_burger_123');
+        close.classList.add('header_burger_close_active');
+    });
+
+    closeItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+        close.classList.remove('header_burger_close_active');
+        openItem.classList.remove('header_burger_123');
+    });
+
+    const menuu = document.querySelector('.header_nav');
+    const menuuBurger = document.querySelector('.header_burger');
+    document.addEventListener('click', (e) => {
+        const click = e.composedPath().includes(menuuBurger);
+        const clickM = e.composedPath().includes(menuu);
+
+       if ( !click && !clickM) {
+           menu.classList.remove('header_nav_active');
+           close.classList.remove('header_burger_close_active');
+           openItem.classList.remove('header_burger_123');
+       }
+    })
+
+    if (window.innerWidth < 1055) {
+        for (let i =0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header_nav_active');
+                close.classList.remove('header_burger_close_active');
+                openItem.classList.remove('header_burger_123');
+            });
+        }
+    }
+
+}());
