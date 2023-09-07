6a9c661d60f874e14cf3b799cba82da53c9fd5da
Dmitriy Bekesh <bekeshdmitriy@yandex.ru>
2023-09-07 21:54:08 +0300

    fix: fix burger menu

--- library/js/main.js
+++ library/js/main.js
@@ -1,59 +1,46 @@
 (function () {
-    const burgerItem = document.querySelector('.burger');
+    const burgerItem = document.querySelector('.header_burger');
     const menu = document.querySelector('.header_nav');
     const close = document.querySelector('.header_burger_close');
     const closeItem = document.querySelector('.header_burger_close');
     const openItem = document.querySelector('.header_burger');
-    
+    const menuLinks = document.querySelectorAll('.nav-link');
+
+    /// открытие меню на клик по иконке
     burgerItem.addEventListener('click', () => {
-        menu.classList.add('header_nav_active');
-        openItem.classList.add('header_burger_123');
-        close.classList.add('header_burger_close_active');
-        console.log(menuLinks);
+       menu.classList.add('header_nav_active');
+       openItem.classList.add('header_burger_123');
+       close.classList.add('header_burger_close_active');
     });
-
-        if (document.documentElement.classList.contains('nav-link')) {
-            console.log(666)
-            };
-
-
-//    closeItem.addEventListener('click', () => {
-//       menu.classList.remove('header_nav_active');
-//        close.classList.remove('header_burger_close_active');
-//        openItem.classList.remove('header_burger_123');
-//    });
-
-//    if (window.innerWidth <= 1054) {
-//      console.log(3);
-//      for (let i =0; i < menuLinks.length; i += 1) {
-//         menuLinks[i].addEventListener('click', () => {
-//             menu.classList.remove('header_nav_active');
-//             close.classList.remove('header_burger_close_active');
-//            openItem.classList.remove('header_burger_123');
-//             console.log(menu);
-//             console.log(1);
-//          });
-//      }
-//  }
-
-    const menuu = document.querySelector('.header_nav');
-    const menuuBurger = document.querySelector('.header_burger');
-    const menuLinks = document.querySelector('.nav-list');
-    document.addEventListener('click', (e) => {
-        const click = e.composedPath().includes(menuuBurger);
-        const clickM = e.composedPath().includes(menuu);
-        const clickMM = e.composedPath().includes(menuLinks);
-
-       if ( !click && !clickM) {
-           menu.classList.remove('header_nav_active');
-           close.classList.remove('header_burger_close_active');
-           openItem.classList.remove('header_burger_123');
-           console.log(click);
-           console.log(clickM);
-           console.log(clickMM);
-       }
+    /// закрытие меню на клик по иконке
+    closeItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+        close.classList.remove('header_burger_close_active');
+        openItem.classList.remove('header_burger_123');
     });
 
 
+    // закрытие меню по клику на ссылку
+  const menuu = document.querySelector('.header_nav'); // меню которые скрыто за пределами
+  const menuuBurger = document.querySelector('.header_burger'); // иконка открытия меню
+  document.addEventListener('click', (e) => {
+      const click = e.composedPath().includes(menuuBurger);
+       const clickM = e.composedPath().includes(menuu);
+     if ( !click && !clickM) {  //если клик не на меню и не на открытие меню то
+         menu.classList.remove('header_nav_active'); // удаляем - меню которые появляется после клика на иконку, хедарнав пропал
+         close.classList.remove('header_burger_close_active');
+         openItem.classList.remove('header_burger_123');
+     }
+  });
+
+   // if (window.innerWidth < 1055) {
+        for (let i =0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+              menu.classList.remove('header_nav_active');
+              close.classList.remove('header_burger_close_active');
+              openItem.classList.remove('header_burger_123');
+            });
+        }
+    //}
 
 }());
