ad1d74b586c871810c67f0984d5c5d5b493fe23c
Dmitriy B <i@bekesh.ru>
2023-09-27 16:28:27 +0300

    feat: add startup pictures

---
+++ image-galery/api.js
@@ -0,0 +1,33 @@
+
+
+
+let keySearch = 'Nizhny Novgorod';
+let perPage = 12;
+
+async function getData(keySearch, perPage) {
+  const url = `https://api.unsplash.com/search/photos?query=${keySearch}&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo&per_page=${perPage}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log('keySearch: ', keySearch);
+  console.log('res: ', res);
+  console.log('data: ', data);
+  console.log('urls: ', data.results[0].urls.regular);
+  console.log('dlina: ', data.results.length)
+  showData(data)
+
+}
+getData(keySearch, perPage);
+
+function showData(data) {
+  const imageDiv = document.querySelector('.image-container');
+
+  for (let i = 0; i < data.results.length; i++) {
+    let img = document.createElement('img');
+    img.classList.add('gallery-img')
+    img.src = `${data.results[i].urls.regular}`;
+    img.alt = `${data.results[i].alt_description}`;
+    imageDiv.append(img);
+    console.log(i, data.results[i].alt_description);
+  }
+
+}
