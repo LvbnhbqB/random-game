9b7665d92334f345c050ec71b0ec2143f94f607d
Dmitriy B <i@bekesh.ru>
2023-09-27 21:11:28 +0300

    feat: add response API

--- image-galery/api.js
+++ image-galery/api.js
@@ -1,6 +1,3 @@
-
-
-
 let keySearch = 'Nizhny Novgorod';
 let perPage = 12;
 
@@ -8,13 +5,13 @@ async function getData(keySearch, perPage) {
   const url = `https://api.unsplash.com/search/photos?query=${keySearch}&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo&per_page=${perPage}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log('keySearch: ', keySearch);
-  console.log('res: ', res);
-  console.log('data: ', data);
-  console.log('urls: ', data.results[0].urls.regular);
-  console.log('dlina: ', data.results.length)
+  //console.log('keySearch: ', keySearch);
+  //console.log('res: ', res);
+  //console.log('data: ', data);
+  //console.log('urls: ', data.results[0].urls.regular);
+  //console.log('dlina: ', data.results.length)
+  document.querySelector('.image-container').innerHTML = '';
   showData(data)
-
 }
 getData(keySearch, perPage);
 
@@ -27,7 +24,16 @@ function showData(data) {
     img.src = `${data.results[i].urls.regular}`;
     img.alt = `${data.results[i].alt_description}`;
     imageDiv.append(img);
-    console.log(i, data.results[i].alt_description);
+    //console.log(i, data.results[i].alt_description);
   }
-
 }
+
+const formElement = document.getElementById('search');
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(formElement); 
+  const request = formData.get('text');
+ // console.log(request);
+  let keySearch = request;
+  getData(keySearch, perPage)
+});
