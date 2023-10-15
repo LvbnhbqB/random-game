a40e6bf9024a0f5d8b1b6c6f54b6fe8986103a2c
Dmitriy B <i@bekesh.ru>
2023-10-15 21:13:51 +0300

    feat: add player choice

--- random-game/game.js
+++ random-game/game.js
@@ -1,2 +1,31 @@
 
 
+let playerScore = document.querySelector('.player-score');
+let pcScore = document.querySelector('.pc-score');
+let playerChoice = document.querySelector('.player-choice');
+let pcChoice = document.querySelector('.pc-choice');
+let buttons = document.querySelectorAll('.choice');
+let countPlayer = 0, countPc = 0;
+
+
+
+function playPlayer(e) {
+console.log('eee');
+  let target = e.target;
+  let player;
+  player = target.dataset.choice;
+  console.log(player);
+  console.log(target);
+}
+
+function playPC() {
+
+}
+
+function win(){
+
+}
+
+playerChoice.addEventListener('click', playPlayer);
+
+console.log(countPlayer);
