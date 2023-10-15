c17f0589d060aca6f856a4283edd615470da02fe
Dmitriy B <i@bekesh.ru>
2023-10-15 21:44:57 +0300

    feat: add function winner

--- random-game/game.js
+++ random-game/game.js
@@ -6,18 +6,17 @@ let playerChoice = document.querySelector('.player-choice');
 let pcChoice = document.querySelector('.pc-choice');
 let buttons = document.querySelectorAll('.choice');
 let countPlayer = 0, countPc = 0;
+let player, pc;
 
 
 
 function playPlayer(e) {
-  playPC();
   let target = e.target;
-  let player;
   player = target.dataset.choice;
+  console.log('player');
 }
 
 function playPC() {
-  let pc;
   let randomPC=Math.floor(Math.random() * 3);
   if (randomPC === 0) {
     pc = 'rock';
@@ -28,12 +27,23 @@ function playPC() {
   if (randomPC === 2) {
     pc = 'scissors';
   }
+  console.log('pc');
 }
 
-function win(){
+function win(e){
+  playPlayer(e);
+  playPC();
+  console.log('win');
+  if (player === pc) {
+    console.log(':(');
+  } else if (((player === 'rock') && (pc === 'scissors')) || ((player === 'scissors') && (pc === 'papper')) || ((player === 'papper') && (pc === 'scissors'))) {
+    console.log('player winner!');
+  } else {
+    console.log('pc winner!')
+  }
 
 }
 
-playerChoice.addEventListener('click', playPlayer);
+playerChoice.addEventListener('click', win);
 
 console.log(countPlayer);
