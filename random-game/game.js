7c58527bf7a58404676230f1564038b944f3e0d7
Dmitriy B <i@bekesh.ru>
2023-10-16 15:55:32 +0300

    feat: add style choice buttons PC

--- random-game/game.js
+++ random-game/game.js
@@ -5,6 +5,9 @@ let pcScore = document.querySelector('.pc-score');
 let playerChoice = document.querySelector('.player-choice');
 let pcChoice = document.querySelector('.pc-choice');
 let buttons = document.querySelectorAll('.choice');
+let buttonsPcR = document.getElementById('pc-r')
+let buttonsPcS = document.getElementById('pc-s')
+let buttonsPcP = document.getElementById('pc-p')
 let winnerText;
 let countPlayer = 0, countPc = 0;
 let player, pc;
@@ -21,12 +24,21 @@ function playPC() {
   let randomPC=Math.floor(Math.random() * 3);
   if (randomPC === 0) {
     pc = 'rock';
+    buttonsPcR.classList.add('active-pc')
+    buttonsPcP.classList.remove('active-pc')
+    buttonsPcS.classList.remove('active-pc')
   }
   if (randomPC === 1) {
     pc = 'papper';
+    buttonsPcP.classList.add('active-pc')
+    buttonsPcR.classList.remove('active-pc')
+    buttonsPcS.classList.remove('active-pc')
   }
   if (randomPC === 2) {
     pc = 'scissors';
+    buttonsPcS.classList.add('active-pc')
+    buttonsPcP.classList.remove('active-pc')
+    buttonsPcR.classList.remove('active-pc')
   }
   console.log('pc');
 }
