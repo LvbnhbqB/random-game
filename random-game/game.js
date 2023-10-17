f640557f7907157c42d8d6d1a66e12dea5d3ed6f
Dmitriy B <i@bekesh.ru>
2023-10-17 11:14:18 +0300

    feat: add style winner

--- random-game/game.js
+++ random-game/game.js
@@ -5,9 +5,11 @@ let pcScore = document.querySelector('.pc-score');
 let playerChoice = document.querySelector('.player-choice');
 let pcChoice = document.querySelector('.pc-choice');
 let buttons = document.querySelectorAll('.choice');
-let buttonsPcR = document.getElementById('pc-r')
-let buttonsPcS = document.getElementById('pc-s')
-let buttonsPcP = document.getElementById('pc-p')
+let buttonsPcR = document.getElementById('pc-r');
+let buttonsPcS = document.getElementById('pc-s');
+let buttonsPcP = document.getElementById('pc-p');
+let winnerImagePlayer = document.getElementById('avatar-player-win');
+let winnerImagePc = document.getElementById('avatar-pc-win');
 let winnerText;
 let countPlayer = 0, countPc = 0;
 let player, pc;
@@ -60,24 +62,35 @@ function win(e){
   playPC();
   if (player === pc) {
     winnerText = document.getElementsByClassName('winner-text')[0].textContent = 'GAME DRAW :(';
+    winnerImagePlayer.classList.remove('win-active')
+    winnerImagePc.classList.remove('win-active')
     audio.src = './assets/media/oops.mp3'
     audio.currentTime = 0;
     audio.play();
   } else if (((player === 'rock') && (pc === 'scissors')) || ((player === 'scissors') && (pc === 'papper')) || ((player === 'papper') && (pc === 'rock'))) {
     winnerText = document.getElementsByClassName('winner-text')[0].textContent = 'PLAYER WIN!!!';
+    winnerImagePlayer.classList.add('win-active')
+    winnerImagePc.classList.remove('win-active')
     countPlayer++;
     document.getElementsByClassName('player-score')[0].textContent = countPlayer;
     audio.src = './assets/media/win.mp3'
     audio.currentTime = 0;
     audio.play();
-
+    setTimeout(() => {
+      winnerImagePlayer.classList.remove('win-active')
+    }, 1000);
   } else {
     winnerText = document.getElementsByClassName('winner-text')[0].textContent = 'COMPUTER WIN!!!';
+    winnerImagePlayer.classList.remove('win-active')
+    winnerImagePc.classList.add('win-active')
     countPc++;
     document.getElementsByClassName('pc-score')[0].textContent = countPc;
     audio.src = './assets/media/loser.mp3'
     audio.currentTime = 0;
     audio.play();
+    setTimeout(() => {
+      winnerImagePc.classList.remove('win-active')
+    }, 1000);
   }
 }
 
