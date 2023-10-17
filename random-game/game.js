bbd92b27c87e10913116873a8fc2b14394e88de3
Dmitriy B <i@bekesh.ru>
2023-10-17 11:15:37 +0300

    refactor: apply prettier

--- random-game/game.js
+++ random-game/game.js
@@ -1,20 +1,17 @@
-
-
-let playerScore = document.querySelector('.player-score');
-let pcScore = document.querySelector('.pc-score');
-let playerChoice = document.querySelector('.player-choice');
-let pcChoice = document.querySelector('.pc-choice');
-let buttons = document.querySelectorAll('.choice');
-let buttonsPcR = document.getElementById('pc-r');
-let buttonsPcS = document.getElementById('pc-s');
-let buttonsPcP = document.getElementById('pc-p');
-let winnerImagePlayer = document.getElementById('avatar-player-win');
-let winnerImagePc = document.getElementById('avatar-pc-win');
+let playerScore = document.querySelector(".player-score");
+let pcScore = document.querySelector(".pc-score");
+let playerChoice = document.querySelector(".player-choice");
+let pcChoice = document.querySelector(".pc-choice");
+let buttons = document.querySelectorAll(".choice");
+let buttonsPcR = document.getElementById("pc-r");
+let buttonsPcS = document.getElementById("pc-s");
+let buttonsPcP = document.getElementById("pc-p");
+let winnerImagePlayer = document.getElementById("avatar-player-win");
+let winnerImagePc = document.getElementById("avatar-pc-win");
 let winnerText;
-let countPlayer = 0, countPc = 0;
+let countPlayer = 0,
+  countPc = 0;
 let player, pc;
-
-
 
 function playPlayer(e) {
   let target = e.target;
@@ -22,76 +19,90 @@ function playPlayer(e) {
 }
 
 function playPC() {
-  let randomPC=Math.floor(Math.random() * 3);
+  let randomPC = Math.floor(Math.random() * 3);
   if (randomPC === 0) {
-    pc = 'rock';
-    buttonsPcR.classList.add('active-pc')
-    buttonsPcP.classList.remove('active-pc')
-    buttonsPcS.classList.remove('active-pc')
+    pc = "rock";
+    buttonsPcR.classList.add("active-pc");
+    buttonsPcP.classList.remove("active-pc");
+    buttonsPcS.classList.remove("active-pc");
     setTimeout(() => {
-      buttonsPcR.classList.remove('active-pc')
-      winnerText = document.getElementsByClassName('winner-text')[0].textContent = "Who's the winner?";
+      buttonsPcR.classList.remove("active-pc");
+      winnerText = document.getElementsByClassName(
+        "winner-text"
+      )[0].textContent = "Who's the winner?";
     }, 1000);
   }
   if (randomPC === 1) {
-    pc = 'papper';
-    buttonsPcP.classList.add('active-pc')
-    buttonsPcR.classList.remove('active-pc')
-    buttonsPcS.classList.remove('active-pc')
+    pc = "papper";
+    buttonsPcP.classList.add("active-pc");
+    buttonsPcR.classList.remove("active-pc");
+    buttonsPcS.classList.remove("active-pc");
     setTimeout(() => {
-      buttonsPcP.classList.remove('active-pc')
-      winnerText = document.getElementsByClassName('winner-text')[0].textContent = "Who's the winner?";
+      buttonsPcP.classList.remove("active-pc");
+      winnerText = document.getElementsByClassName(
+        "winner-text"
+      )[0].textContent = "Who's the winner?";
     }, 1000);
   }
   if (randomPC === 2) {
-    pc = 'scissors';
-    buttonsPcS.classList.add('active-pc')
-    buttonsPcP.classList.remove('active-pc')
-    buttonsPcR.classList.remove('active-pc')
+    pc = "scissors";
+    buttonsPcS.classList.add("active-pc");
+    buttonsPcP.classList.remove("active-pc");
+    buttonsPcR.classList.remove("active-pc");
 
     setTimeout(() => {
-      buttonsPcS.classList.remove('active-pc')
-      winnerText = document.getElementsByClassName('winner-text')[0].textContent = "Who's the winner?";
+      buttonsPcS.classList.remove("active-pc");
+      winnerText = document.getElementsByClassName(
+        "winner-text"
+      )[0].textContent = "Who's the winner?";
     }, 1000);
   }
 }
 
-function win(e){
+function win(e) {
   const audio = new Audio();
   playPlayer(e);
   playPC();
   if (player === pc) {
-    winnerText = document.getElementsByClassName('winner-text')[0].textContent = 'GAME DRAW :(';
-    winnerImagePlayer.classList.remove('win-active')
-    winnerImagePc.classList.remove('win-active')
-    audio.src = './assets/media/oops.mp3'
+    winnerText = document.getElementsByClassName("winner-text")[0].textContent =
+      "GAME DRAW :(";
+    winnerImagePlayer.classList.remove("win-active");
+    winnerImagePc.classList.remove("win-active");
+    audio.src = "./assets/media/oops.mp3";
     audio.currentTime = 0;
     audio.play();
-  } else if (((player === 'rock') && (pc === 'scissors')) || ((player === 'scissors') && (pc === 'papper')) || ((player === 'papper') && (pc === 'rock'))) {
-    winnerText = document.getElementsByClassName('winner-text')[0].textContent = 'PLAYER WIN!!!';
-    winnerImagePlayer.classList.add('win-active')
-    winnerImagePc.classList.remove('win-active')
+  } else if (
+    (player === "rock" && pc === "scissors") ||
+    (player === "scissors" && pc === "papper") ||
+    (player === "papper" && pc === "rock")
+  ) {
+    winnerText = document.getElementsByClassName("winner-text")[0].textContent =
+      "PLAYER WIN!!!";
+    winnerImagePlayer.classList.add("win-active");
+    winnerImagePc.classList.remove("win-active");
     countPlayer++;
-    document.getElementsByClassName('player-score')[0].textContent = countPlayer;
-    audio.src = './assets/media/win.mp3'
+    document.getElementsByClassName("player-score")[0].textContent =
+      countPlayer;
+    audio.src = "./assets/media/win.mp3";
     audio.currentTime = 0;
     audio.play();
     setTimeout(() => {
-      winnerImagePlayer.classList.remove('win-active')
+      winnerImagePlayer.classList.remove("win-active");
     }, 1000);
   } else {
-    winnerText = document.getElementsByClassName('winner-text')[0].textContent = 'COMPUTER WIN!!!';
-    winnerImagePlayer.classList.remove('win-active')
-    winnerImagePc.classList.add('win-active')
+    winnerText = document.getElementsByClassName("winner-text")[0].textContent =
+      "COMPUTER WIN!!!";
+    winnerImagePlayer.classList.remove("win-active");
+    winnerImagePc.classList.add("win-active");
     countPc++;
-    document.getElementsByClassName('pc-score')[0].textContent = countPc;
-    audio.src = './assets/media/loser.mp3'
+    document.getElementsByClassName("pc-score")[0].textContent = countPc;
+    audio.src = "./assets/media/loser.mp3";
     audio.currentTime = 0;
     audio.play();
     setTimeout(() => {
-      winnerImagePc.classList.remove('win-active')
+      winnerImagePc.classList.remove("win-active");
     }, 1000);
   }
 }
 
-playerChoice.addEventListener('click', win);
+playerChoice.addEventListener("click", win);
